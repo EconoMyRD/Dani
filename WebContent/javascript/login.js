d2f27836949022937097332d3933c46cd5374a19
DanielaMara <danielamaradms@gmail.com>
2015-06-10 02:05:07 -0300

    Adequaçoes de pastas

---
+++ WebContent/javascript/login.js
@@ -0,0 +1,31 @@
+
+/*Login Menu*/
+$("#logar").live('click', function () 
+{
+    $("#div-logar").hide();
+    $("#div-logar").fadeIn('slow');
+});
+
+
+/*Carregar Menu para outras páginas*/
+$(document).ready(function()
+{  
+    $("#header").load("header.html");  
+    $("#footer").load("footer.html");
+    $("#lancamentos").load("lancamento.html");
+    $("#relatorios").load("relatorio.html");
+}); 
+
+
+/*Carregar páginas com o clique*/
+$(document).ready(function() 
+{
+    $('.menu-interno').click(function() 
+    {
+        $("#conteudo").load($(this).attr('href'));
+        return false;
+    });    
+});
+
+
+
